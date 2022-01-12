Fix: incorrectly implemented input_vector computation

src/test/machine_learning/input_vector_test.py
@@ -9,19 +9,25 @@ import unittest
 
 from unittest.mock import Mock
 
+from src.game.object_builder import Snake
 from src.machine_learning.input_vector import InputVector
+from src.utils.drawing_manager import Canvas
 
 
 class TestInputVector(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.snake = Mock()
-        self.snake.width = 100
-        self.snake.height = 100
-        self.snake.step_size = 5
+        self.snake = Snake(
+            width=100,
+            height=100,
+            step_size=5,
+            length=5,
+            x_init=0,
+            y_init=0,
+            boundary=False
+        )
 
     def test_respect_to_apple(self):
-
         self.snake.x = [50]
         self.snake.y = [50]
         self.snake.length = 1
@@ -32,6 +38,15 @@ class TestInputVector(unittest.TestCase):
         self.assertTrue(np.array_equal(vector, np.array([1, 0, 1, 0])))
 
     def test_right_side_not_clear(self):
+        self.snake.x = [30, 30, 30, 35, 35, 35]
+        self.snake.y = [5, 10, 15, 15, 10, 5]
+        self.snake.length = len(self.snake.x)
+
+        iv = InputVector(snake=self.snake, apple=Mock())
+        clear_right_side = iv.right_side_clear()
+        self.assertFalse(clear_right_side)
+
+    def test_right_side_not_clear_periodic(self):
         self.snake.x = [95, 100, 100, 100, 0, 0, 0]
         self.snake.y = [5, 5, 10, 15, 15, 10, 5]
         self.snake.length = len(self.snake.x)
@@ -48,9 +63,17 @@ class TestInputVector(unittest.TestCase):
         clear_left_edge = iv.left_side_clear()
         self.assertFalse(clear_left_edge)
 
+    def test_left_side_not_clear_periodic(self):
+        self.snake.x = [0, 0, 0, 100, 100, 100]
+        self.snake.y = [5, 10, 15, 15, 10, 5]
+        self.snake.length = len(self.snake.x)
+        iv = InputVector(snake=self.snake, apple=Mock())
+        clear_left_edge = iv.left_side_clear()
+        self.assertFalse(clear_left_edge)
+
     def test_top_side_not_clear(self):
-        self.snake.x = [5, 5, 10, 15, 15, 10, 5]
-        self.snake.y = [95, 100, 100, 100, 0, 0, 0]
+        self.snake.x = [0, 5, 10, 15, 15, 10, 5, 0]
+        self.snake.y = [95, 95, 95, 95, 0, 0, 0, 0]
         self.snake.length = len(self.snake.x)
         iv = InputVector(snake=self.snake, apple=Mock())
         clear_top_edge = iv.top_side_clear()
@@ -58,11 +81,19 @@ class TestInputVector(unittest.TestCase):
 
     def test_bottom_side_not_clear(self):
         self.snake.x = [5, 10, 15, 15, 10, 5]
-        self.snake.y = [35, 35, 35, 30, 30, 30]
+        self.snake.y = [30, 30, 30, 35, 35, 35]
         self.snake.length = len(self.snake.x)
         iv = InputVector(snake=self.snake, apple=Mock())
         clear_bottom_side = iv.bottom_side_clear()
         self.assertFalse(clear_bottom_side)
+
+    def test_bottom_side_not_clear_periodic(self):
+        self.snake.x = [0, 5, 10, 15, 15, 10, 5, 0]
+        self.snake.y = [0, 0, 0, 0, 95, 95, 95, 95]
+        self.snake.length = len(self.snake.x)
+        iv = InputVector(snake=self.snake, apple=Mock())
+        clear_bottom_edge = iv.bottom_side_clear()
+        self.assertFalse(clear_bottom_edge)
 
 
 if __name__ == '__main__':