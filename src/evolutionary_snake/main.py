e1dee0b28a6f551142ffdbcf12df46bb08d97cff
Stef van der Meulen <stefvandermeulen@gmail.com>
2025-04-19 22:18:24 +0200

    feature: add cli

--- src/evolutionary_snake/main.py
+++ src/evolutionary_snake/main.py
@@ -1,30 +1,17 @@
 """Main entry point to run evolutionary-snake"""
-import enum
+from evolutionary_snake import enums
 
 
-class GameMode(enum.StrEnum):
-    """Enum to define game modes."""
-    HUMAN_PLAYER = "human_player"
-    TRAINING = "train"
-    AI_PLAYER = "ai_player"
-
-
-def main(
-        mode: GameMode,
-) -> bool:
+def main(game_mode: enums.GameMode) -> None:
     """Main entry point to run evolutionary-snake."""
-    if mode == GameMode.HUMAN_PLAYER:
+    if game_mode == enums.GameMode.HUMAN_PLAYER:
         print("Human player starting...")
-    elif mode == GameMode.TRAINING:
+    elif game_mode == enums.GameMode.TRAINING:
         print("Training starting...")
-    elif mode == GameMode.AI_PLAYER:
+    elif game_mode == enums.GameMode.AI_PLAYER:
         print("AI player starting...")
     else:
-        print(f"Invalid mode {mode} given")
-    return True
-
-
-if __name__ == "__main__":
-    game_mode = GameMode.HUMAN_PLAYER
-    print("=" * 20, f" running mode: {game_mode} ", "=" * 20)
-    main(mode=game_mode)
+        print(
+            f"Invalid game-mode '{game_mode}' given. "
+            f"Choose from {[e.value for e in enums.GameMode]}"
+        )
